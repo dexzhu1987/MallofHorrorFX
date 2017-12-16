ab861a93cee954e380eb0822541e55574b358723
dexzhu1987 <dexzhu1987@hotmail.com>
2017-12-15 17:39:24 -0800

    set up main controller

---
+++ src/Controller/numberWindow.java
@@ -0,0 +1,93 @@
+package Controller;
+
+import java.util.ArrayList;
+import java.util.List;
+import javafx.stage.*;
+import javafx.scene.*;
+import javafx.scene.layout.*;
+import javafx.scene.control.*;
+import javafx.geometry.*;
+
+public class NumberWindow {
+    static int theNumber;
+
+    public static int display(List<Integer> opitons){
+        Stage window = new Stage();
+        window.initModality(Modality.APPLICATION_MODAL);
+        window.setTitle("Please select your number");
+        window.setMinWidth(250);
+        Label label = new Label();
+        label.setText("Please select your number: ");
+
+        Button numberOneButton = new Button("1");
+        Button numberTwoButton = new Button("2");
+        Button numberThreeButton = new Button("3");
+        Button numberFourButton = new Button("4");
+        Button numberFiveButton = new Button("5");
+        Button numberSixButton = new Button("6");
+
+        //Clicking will set answer and close window
+        numberOneButton.setOnAction(e -> {
+            theNumber = 1;
+            window.close();
+        });
+        numberTwoButton.setOnAction(e -> {
+            theNumber = 2;
+            window.close();
+        });
+        numberThreeButton.setOnAction(e -> {
+            theNumber = 3;
+            window.close();
+        });
+        numberFourButton.setOnAction(e -> {
+            theNumber = 4;
+            window.close();
+        });
+        numberFiveButton.setOnAction(e -> {
+            theNumber = 5;
+            window.close();
+        });
+        numberSixButton.setOnAction(e -> {
+            theNumber = 6;
+            window.close();
+        });
+
+
+        List<Button> oneToSix = new ArrayList<>();
+        oneToSix.add(numberOneButton);
+        oneToSix.add(numberTwoButton);
+        oneToSix.add(numberThreeButton);
+        oneToSix.add(numberFourButton);
+        oneToSix.add(numberFiveButton);
+        oneToSix.add(numberSixButton);
+
+        List<Button> optionsButton = new ArrayList<>();
+        for (int i=0; i<opitons.size();i++){
+            for (int q=0; q<oneToSix.size(); q++)
+            if (opitons.get(i)==Integer.parseInt(oneToSix.get(q).getText()) ){
+                optionsButton.add(oneToSix.get(q));
+            }
+        }
+
+
+
+        VBox layout = new VBox(10);
+
+        //Add buttons
+        layout.getChildren().add(label);
+
+        for (int i=0; i<optionsButton.size(); i++){
+            layout.getChildren().add(optionsButton.get(i));
+        }
+
+
+        layout.setAlignment(Pos.CENTER);
+        Scene scene = new Scene(layout);
+        window.setScene(scene);
+        window.showAndWait();
+
+
+        return theNumber;
+    }
+
+}
--- src/View/Main.java
+++ src/View/Main.java
@@ -1,28 +1,98 @@
 package View;
-import Controller.MainController;
-import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
-import javafx.stage.Stage;
+import Controller.NumberWindow;
+import javafx.application.*;
+import javafx.stage.*;
+import javafx.scene.*;
+import javafx.scene.layout.*;
+import javafx.scene.control.*;
+
+import java.awt.event.ActionEvent;
+import java.util.*;
 
 public class Main extends Application{
-
-    @Override
-    public void start(Stage primaryStage) throws Exception{
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("../View/Main.fxml"));
-        Parent root = (Parent) loader.load();
-        MainController ctrl = loader.getController();
-        Scene newScene = new Scene(root);
-        Stage newStage = new Stage();
-        newStage.setScene(newScene);
-        newStage.show();
-
-    }
-
+    Button startGame,howTOPlay, viewRoomButton, viewItemListButton;
+    List<Scene> playersenes = new ArrayList<>();
+    Scene welcomeScene, playerRedScene, playerYellowScene, playerBlueScene, playerGreenScene, playerBrownScene, playerBlackScene;
+    Stage mainWindow;
+    int numberOfPlayers;
 
     public static void main(String[] args) {
         launch(args);
     }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+
+
+        //mainWidow setup
+        mainWindow = primaryStage;
+        mainWindow.setTitle("Mall of Horror");
+
+        //startgameScene
+        List<Integer> numberOfPlayersOptions = new ArrayList<>();
+        numberOfPlayersOptions.add(4);
+        numberOfPlayersOptions.add(5);
+        numberOfPlayersOptions.add(6);
+
+
+        startGame = new Button();
+        startGame.setText("Start Game");
+        startGame.setOnAction(event -> {
+            numberOfPlayers = NumberWindow.display(numberOfPlayersOptions);
+            mainWindow.setScene(welcomeScene);
+        });
+
+
+        howTOPlay = new Button();
+        howTOPlay.setText("How to Play");
+
+        BorderPane firstlayout = new BorderPane();
+        firstlayout.getChildren().addAll(startGame, howTOPlay);
+        Scene firstscene = new Scene(firstlayout, 800, 600);
+        primaryStage.setScene(firstscene);
+        primaryStage.show();
+
+
+        //welcomehowManyPlayerScene;
+
+        BorderPane welcomePlayerslayout = new BorderPane();
+        welcomePlayerslayout.getChildren().addAll();
+        Scene welcomePlayers = new Scene(firstlayout, 800, 600);
+
+
+
+
+
+
+        //playerRedScene;
+        viewRoomButton = new Button();
+        viewRoomButton.setText("Rooms");
+
+        viewItemListButton = new Button();
+        viewItemListButton.setText("Items");
+
+        BorderPane playerRedLayout = new BorderPane();
+        playerRedLayout.getChildren().addAll(viewRoomButton,viewItemListButton);
+
+
+
+
+
+
+
+
+
+
+        Scene playerRedScene;
+
+
+
+
+
+
+    }
+
+
+
 }
 
